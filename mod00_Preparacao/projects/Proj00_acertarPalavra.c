f9bf54d8ae5157b3d1dc56cbf2b422f24f2105ec
edvaldovunge <costaedy18@gmail.com>
2026-07-20 22:48:24 +0100

    update and restart the course

---
+++ mod00_Preparacao/projects/Proj00_acertarPalavra.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Criar um programa que pergunta ao user quantas letras deve ter a palavra, 
+e a segunda pergunta sugestão de uma letra, 
+e o programa deve tentar adivinhar qual é a 
+palavra pensada pelo user(palavra essa que deve ser pré-definida).
+*/
+
+int main(void) {
+
+    return (0);
+}
+
